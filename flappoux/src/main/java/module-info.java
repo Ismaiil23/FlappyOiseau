8c94dae7a426067909b1f167b4e633b929fe6a48
Ismaiil23 <mkisma84@gmail.com>
2022-12-26 22:13:44 +0100

    Ri1

--- flappoux/src/main/java/module-info.java
+++ flappoux/src/main/java/module-info.java
@@ -8,6 +8,7 @@ module com.example.flappoux {
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires com.almasb.fxgl.all;
+    requires javafx.media;
 
     opens com.example.flappoux to javafx.fxml;
     exports com.example.flappoux;
